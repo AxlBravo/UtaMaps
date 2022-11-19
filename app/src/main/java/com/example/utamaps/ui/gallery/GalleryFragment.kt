501821ea52e1f9dee9e8ee19d4b9363a18d45ebf
Axl <AxlBravo.A@gmail.com>
2022-11-19 19:06:55 -0300

    a

--- app/src/main/java/com/example/utamaps/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/utamaps/ui/gallery/GalleryFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.utamaps.databinding.FragmentGalleryBinding
+import com.example.utamaps.databinding.FragmentDeptosBinding
 
 class GalleryFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentDeptosBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,10 +25,10 @@ class GalleryFragment : Fragment() {
         val galleryViewModel =
             ViewModelProvider(this).get(GalleryViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentDeptosBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
+        val textView: TextView = binding.textDeptos
         galleryViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
